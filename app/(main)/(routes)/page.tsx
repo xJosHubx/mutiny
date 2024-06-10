9f6d7eea59d8fa4ff0967947e8fb473cd79902c5
xJosHubx <hhjoshhh@gmail.com>
2024-06-09 21:45:33 -0400

    Next.js middleware setup and commit

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,14 +1,3 @@
-import { Button } from "@/components/ui/button";
-
-const state = true;
-
 export default function Home() {
-    return (
-        <div>
-            <p className="text-3xl font-bold text-indigo-500">
-                Hello, I'm Mutiny
-            </p>
-            <Button>Click Me!</Button>
-        </div>
-    );
+  return <div>This is a protected route!</div>;
 }
