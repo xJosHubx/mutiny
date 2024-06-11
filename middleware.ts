7cd3ebedef144ec46b85e98047ad726bc549d19a
xJosHubx <hhjoshhh@gmail.com>
2024-06-11 14:41:32 -0400

    This middleware file contains the code that fixed the redirect issue I was having with sign in. Learned more about protected routes.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// Add '/' to the list of public routes
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/dashboard(.*)",
+  "/profile(.*)",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
