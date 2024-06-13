1e5e61d65e69550659187eec1d214c895b4a48e7
xJosHubx <hhjoshhh@gmail.com>
2024-06-13 15:15:48 -0400

    Add suppresHydrationWarning to app>layout>rootlayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={font.className}>
           <ThemeProvider
             attribute="class"
