d90a5528b968f080f809e48848128593d23d5422
xJosHubx <hhjoshhh@gmail.com>
2024-06-13 17:16:43 -0400

    Fix: them.provider to theme.provider.tsx

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/providers/them.provider";
+import { ThemeProvider } from "@/components/providers/theme.provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
