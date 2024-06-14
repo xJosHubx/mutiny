f6a17875888e4a9209d3ff52e931331db1ed13d7
xJosHubx <hhjoshhh@gmail.com>
2024-06-14 00:42:02 -0400

    More reverting trying to get theme mode out of the moon icon div.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme.provider";
-import { cn } from "/lib/utils";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
