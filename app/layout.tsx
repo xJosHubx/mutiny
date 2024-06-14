e2234b77faa1b12997063008076e84bb59138d5f
xJosHubx <hhjoshhh@gmail.com>
2024-06-14 00:43:59 -0400

    Jesus fixed because I asked him. typo in body in hex. Used 5 digits instead of 6. Thank You Jesus.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn(font.className, "bg-white dark:bg-[#31338]")}>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
