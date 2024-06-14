fdd432ee125ef9eb81cf9eedbc818651fa1ad751
xJosHubx <hhjoshhh@gmail.com>
2024-06-14 00:18:26 -0400

    //If issue with system mode staying dark or light, then reset to "enableSystem={false}"

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            enableSystem={false}
+            enableSystem //If issue with system mode staying dark or light, then reset to "enableSystem={false}"
             storageKey="discord-theme">
             {children}
           </ThemeProvider>
