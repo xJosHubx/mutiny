40ad08eb294375020a5d13e44cfd2947c9f08f51
xJosHubx <hhjoshhh@gmail.com>
2024-06-17 14:17:01 -0400

    Added post.ts, cient.ts, schema.prisma updated, seed.ts, migration-lock, migration.sql Got db up and running.

---
+++ prisma/migrations/20240617045254_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
