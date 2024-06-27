2f4e35e4114e920bb92f2088bd76928788c4a179
xJosHubx <hhjoshhh@gmail.com>
2024-06-27 03:28:44 -0400

    Had big problems with UploadDropzone statement. Removed @/lib/uploadthing and then manually used gui to re-enter the file path.

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,24 @@
 "use client";
 
-export const FileUpload = () => {
-  return <div>File Upload Component</div>;
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+}
+
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
 };
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -81,7 +81,11 @@ export const InitialModal = () => {
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
-                        <FileUpload />
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
                       </FormControl>
                     </FormItem>
                   )}
