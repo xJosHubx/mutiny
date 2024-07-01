b0ecc33d715baf479150ea4d3099c095cc77df76
xJosHubx <hhjoshhh@gmail.com>
2024-06-30 21:37:07 -0400

    Created modal-provider.tsx and create-server-modal.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+
 import { ThemeProvider } from "@/components/providers/theme.provider";
 import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -27,6 +29,8 @@ export default function RootLayout({
             enableSystem //If issue with system mode staying dark or light, then reset to "enableSystem={false}"
             storageKey="discord-theme">
             {children}
+
+            <ModalProvider />
           </ThemeProvider>
         </body>
       </html>
