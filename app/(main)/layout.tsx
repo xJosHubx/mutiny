12fa0de9e3e6e57d23c322bad7da44ef1da93b20
xJosHubx <hhjoshhh@gmail.com>
2024-07-15 22:29:11 -0400

    Bug Fix: App> (main) > layout.tsx on line 6 has a z-index. It was previously set to 20 which ended up being too low. We now have it set to 30 and the tooltips are working as expected. Yay!!

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,9 @@ import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full w-[72px] z-20 flex-col fixed inset-y-0 ">
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0 ">
+        {" "}
+        {/* Don't touch z value. Less than 30 makes tooltip disappear!!! */}
         <NavigationSidebar />
       </div>
       <main className="md:pl-[72px] h-full">{children}</main>
