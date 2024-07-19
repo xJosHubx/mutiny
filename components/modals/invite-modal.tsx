3234a9ab8267aa5ba18d6772a83e4d1355bf34ea
xJosHubx <hhjoshhh@gmail.com>
2024-07-18 20:16:15 -0400

    Created route.ts for serverId > invite-code > route.ts InviteModal still not working, mf. As pissed off as I am that I haven't figured this out yet, I know I will be twice as happy when I do. Plus I will have learned a lot.

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Copy, RefreshCw } from "lucide-react";
+import axios from "axios";
+import { useState } from "react";
+import { Check, Copy, RefreshCw } from "lucide-react";
 
 import {
   Dialog,
@@ -12,17 +14,44 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
-import { useOrigin } from "@/hooks/use-origin";
 import { useModal } from "@/hooks/use-modal-store";
+import { useOrigin } from "@/hooks/use-origin";
 
 export const InviteModal = () => {
-  const { isOpen, onClose, type, data } = useModal();
+  const { onOpen, isOpen, onClose, type, data } = useModal();
   const origin = useOrigin();
 
   const isModalOpen = isOpen && type === "invite";
   const { server } = data;
 
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const onNew = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.patch(
+        `/api/servers/${server?.id}/invite-code`
+      );
+
+      onOpen("invite", { server: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -31,23 +60,31 @@ export const InviteModal = () => {
         aria-describedby={undefined}>
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Invite a Friend!
+            Invite Friends!
           </DialogTitle>
         </DialogHeader>
         <div className="p-6">
           <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70 ">
-            Invite Modal!
+            Server Invite Link
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
             <Input
+              disabled={isLoading}
               className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-              defaultValue={inviteUrl}
+              value={inviteUrl}
+              readOnly
             />
-            <Button size="icon">
-              <Copy className="w-4 h-4" />
+            <Button disabled={isLoading} onClick={onCopy} size="icon">
+              {copied ? (
+                <Check className="w-4 h-4" />
+              ) : (
+                <Copy className="w-4 h-4" />
+              )}
             </Button>
           </div>
           <Button
+            onClick={onNew}
+            disabled={isLoading}
             variant="link"
             size="sm"
             className="text-xs text-zinc-500 mt-4">
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
