7999cbcf1e530d94e2baf4499873ddb55c61e095
xJosHubx <hhjoshhh@gmail.com>
2024-07-29 19:14:53 -0400

    Created editServerModal Updated ModalProvider & useModalStore

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 
@@ -20,6 +21,7 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   );
 };
