27baee16784292876f0b5e4ab8615f4160c61ac8
xJosHubx <hhjoshhh@gmail.com>
2024-08-18 15:09:47 -0400

    Created server-search.tsx & updated related files. Created command.tsx.

--- components/modals/delete-server-modal.tsx
+++ components/modals/delete-server-modal.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import axios from "axios";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
 import {
   Dialog,
@@ -12,17 +12,20 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { useModal } from "@/hooks/use-modal-store";
+
 import { Button } from "@/components/ui/button";
+import { useModal } from "@/hooks/use-modal-store";
 
 export const DeleteServerModal = () => {
-  const { isOpen, onClose, type, data } = useModal();
+  const { isOpen, onClose, type, data = {} } = useModal();
+
   const router = useRouter();
 
-  const isModalOpen = isOpen && type === "deleteServer";
-  const { server } = data;
-
   const [isLoading, setIsLoading] = useState(false);
+
+  const isModalOpen = isOpen && type === "deleteServer";
+
+  const { server } = data || {};
 
   const onClick = async () => {
     try {
@@ -32,7 +35,7 @@ export const DeleteServerModal = () => {
 
       onClose();
       router.refresh();
-      //router.push("/");
+      router.push("/");
     } catch (error) {
       console.log(error);
     } finally {
@@ -42,22 +45,21 @@ export const DeleteServerModal = () => {
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black rounded-xl p-5 overflow-hidden">
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            ¡Delete Server!
+            Delete Server
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Are you sure?
-            <br />
-            <span className="text-indigo-500 font-semibold">
+          <DialogDescription className="text-zinc-500 text-center">
+            Are you sure you want to do this? <br />
+            <span className="font-semibold text-indigo-500">
               {server?.name}
             </span>{" "}
-            will be permanently deleted!!!
+            will be permanently deleted.
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="bg-gray-100 px-6 py-4">
-          <div className="flex itmes-center justify-between w-full">
+          <div className="flex items-center justify-between w-full">
             <Button disabled={isLoading} onClick={onClose} variant="ghost">
               Cancel
             </Button>
