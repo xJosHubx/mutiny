a986ea3385d4823c5932f205b7ed9d9c31b02d4d
xJosHubx <hhjoshhh@gmail.com>
2024-09-30 09:13:27 -0400

    Added server-section.tsx just below search bar. Used component in Server Sidebar.

---
+++ components/server/server-section.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const ServerSection = () => {
+  return <div>Server Section</div>;
+};
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,13 +1,15 @@
 import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { SelectSeparator } from "@/components/ui/select";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
 import { ServerHeader } from "./server-header";
 import { ServerSearch } from "./server-search";
-import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { ServerSection } from "./server-section";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -122,6 +124,12 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             ]}
           />
         </div>
+        <SelectSeparator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection />
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
